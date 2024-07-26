10a072ed9895181a554ad39f3caa014492a2d241
MD. Muhiminul Haque <apon2041@gmail.com>
2024-07-26 17:47:57 +0600

    Update FrontendController.php

--- src/FrontendController.php
+++ src/FrontendController.php
@@ -7,30 +7,27 @@ namespace Aponahmed\StaticPageGenerator;
  *
  * @author Mahabub
  */
-class FrontendController {
+class FrontendController
+{
 
     private $options;
 
     //put your code here
-    public function __construct($options) {
+    public function __construct($options)
+    {
         $this->options = $options;
-        $gMode = get_option('staticGmood');
-        //var_dump($gMode);
-        //exit;
-        if (!is_admin()) {//Testing 
-            //sleep(10);
-        }
-        if ($gMode !== '1' && $this->options['listGmood'] == '0') {
+        // $gMode = get_option('staticGmood');
+        // if ($gMode !== '1' && $this->options['listGmood'] == '0') {
             add_action('template_redirect', [$this, 'disableCustomPostType']);
-        }
+        //}
     }
 
-    function disableCustomPostType($template) {
+    function disableCustomPostType($template)
+    {
         $post = get_queried_object();
         if ($post instanceof \WP_Post && $post->post_type == $this->options['postType']) {
             wp_redirect(home_url());
         }
         return $template;
     }
-
 }
